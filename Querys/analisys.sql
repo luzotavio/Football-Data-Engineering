e28661a2de1aae86fa09d82d7105645fede70c5a
Luiz Otávio <139489817+luzotavio@users.noreply.github.com>
2026-04-28 10:48:20 -0300

    adicionando análises no sql

--- Querys/analisys.sql
+++ Querys/analisys.sql
@@ -40,3 +40,39 @@ FROM stadiums
 GROUP BY region, capacity,stadium
 ) t
 WHERE capacity_rank <= 3
+
+-- stadiums with capacity above the average
+
+SELECT * FROM (
+    SELECT
+        stadium,
+        region,
+        capacity,
+        AVG(capacity) OVER(PARTITION BY region) as avg_region_capacity
+    FROM stadiums
+) t
+WHERE capacity > avg_region_capacity;
+
+
+-- stadiumns with closes capacity to regional median
+WITH MedianCalculation AS (
+    SELECT
+        stadium,
+        region,
+        capacity,
+        PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY capacity)
+            OVER (PARTITION BY region) AS regional_median
+    FROM stadiums
+),
+DistanceCalculation AS (
+    SELECT
+        *,
+        ABS(capacity - regional_median) AS distance_to_median
+    FROM MedianCalculation
+)
+SELECT * FROM DistanceCalculation
+ORDER BY distance_to_median
+
+
+
+
